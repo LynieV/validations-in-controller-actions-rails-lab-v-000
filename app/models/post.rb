015d72549d18586f4482c202daffc11ec10c4c7b
Lynie McClimon <vmcclimon33@yahoo.com>
2019-12-20 01:39:19 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates :category, inclusion: { in: %w(fiction nonfiction),
+    message: "%{value} is not valid please provide either fiction or nonfiction" }
 end
